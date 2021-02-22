a642e1a7a3071c5028efffe90dd9ccc5199be7fe
Aeryn\RedFraggle <vdMehden@gmail.com>
2021-02-22 18:20:45 +0100

    add missing MSVC include

--- src/caffe/util/io.cpp
+++ src/caffe/util/io.cpp
@@ -18,6 +18,7 @@
 #include "caffe/common.hpp"
 #include "caffe/proto/caffe.pb.h"
 #include "caffe/util/io.hpp"
+#include <corecrt_io.h>
 
 const int kProtoReadBytesLimit = INT_MAX;  // Max size of 2 GB minus 1 byte.
 
